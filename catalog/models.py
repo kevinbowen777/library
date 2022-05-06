99be3ec072762e3402f1a4c7d2817f0ce61d39f7
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-06 09:06:26 -0700

    Add pubdate to Book class

--- catalog/models.py
+++ catalog/models.py
@@ -16,6 +16,7 @@ class Genre(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
+    pubdate = models.DateField(null=True, blank=True, help_text="Date Published")
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book.")
     isbn = models.CharField(
         "ISBN",
@@ -62,3 +63,19 @@ class BookInstance(models.Model):
 
     def __str__(self):
         return f"{self.id} ({self.book.title})"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def get_absolute_url(self):
+        return reverse("author-detail", args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
