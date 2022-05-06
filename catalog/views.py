8a966871e2d9dda6076d5ac06c6d90560910faf6
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-06 16:39:46 -0700

    Create index view

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render  # noqa:F401
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Author, Book, BookInstance, Genre  # noqa:F401
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.count()
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+    }
+
+    return render(request, "index.html", context=context)
