6ed68b6ea24b1ae0bf3fb9df4b42bcce1ef71db4
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-06 13:23:38 -0700

    Create AuthorAdmin model

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,21 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre, Language
 
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+
+    list_display = ("last_name", "first_name", "middle_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "middle_name", "last_name", ("date_of_birth", "date_of_death")]
