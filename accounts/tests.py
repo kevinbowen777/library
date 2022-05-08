6865127e1816f7c048c78afd3b0d637fbb415587
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-07 23:25:18 -0700

    Write tests for user accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase  # noqa:F401
+from django.contrib.auth import get_user_model
+from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username="kevin", email="kevin@example.com", password="t3sTp@sS123!")
+        self.assertEqual(user.username, "kevin")
+        self.assertEqual(user.email, "kevin@example.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin", email="superadmin@example.com", password="Te5tp@ss123"
+        )
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
