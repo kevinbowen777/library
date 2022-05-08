dab700b2ab55c0444e596f6634f0df50e9b757ee
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-08 00:29:47 -0700

    Add auth URLs

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,11 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Application specific
     path("catalog/", include("catalog.urls")),
     path("", RedirectView.as_view(url="catalog/", permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
