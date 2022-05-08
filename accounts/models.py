2aadcbda1fea8e63ca174c945834539584224b40
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-08 13:19:08 -0700

    Create librarians group

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser, Group
 from django.db import models  # noqa:F401
 
 library_members, created = Group.objects.get_or_create(name="Library Members")
+librarians, created = Group.objects.get_or_create(name="Librarians")
 
 
 class CustomUser(AbstractUser):
