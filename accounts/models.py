454489c4e345bde28d0c4cd9de00d02faae69b32
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-08 02:06:27 -0700

    Create library_members group

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models  # noqa:F401
+
+library_members, created = Group.objects.get_or_create(name="Library Members")
 
 
 class CustomUser(AbstractUser):
