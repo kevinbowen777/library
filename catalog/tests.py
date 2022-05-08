05cbd65c54674ea175cbd3f02117e2d0851a03c1
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-08 01:41:15 -0700

    Write homepage tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase  # noqa:F401
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(TestCase):
+    def test_index_status_code(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 301)
+
+    def test_index_url_name(self):
+        response = self.client.get(reverse("index"))
+        self.assertEqual(response.status_code, 200)
