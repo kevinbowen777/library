9d8a73c7a568619138f809ea42c36e782ca51159
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-09 13:06:54 -0700

    Add test for overdue BookInstance

--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,7 @@
 import uuid
-from datetime import date  # noqa:F401
+from datetime import date
 
 from django.contrib.auth import get_user_model
-
-# from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 
@@ -70,6 +68,12 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     borrower = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ("m", "Maintenance"),
