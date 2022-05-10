7ad5e53ca6000dace8189282e2d98d32174c7dfc
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-09 22:43:26 -0700

    Verify data is clean

--- catalog/forms.py
+++ catalog/forms.py
@@ -6,4 +6,13 @@ from django.utils.translation import gettext_lazy as _  # noqa:F401
 
 
 class RenewBookForm(forms.Form):
+    """Form for a librarian to renew books."""
+
     renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
+
+    def clean_renewal_date(self):
+        data = self.cleaned_data["renewal_date"]
+
+        # Check date is not in the past.
+        if data < datetime.date.today():
+            raise ValidationError(_("Invalid date - renewal in past"))
