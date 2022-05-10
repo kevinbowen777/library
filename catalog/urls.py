93e5ec7ea8ca1c9348026456539b567fe94fa360
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-10 12:19:06 -0700

    Add book renewal path

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     #
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path(r"borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),
+    #
+    path("book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"),
 ]
