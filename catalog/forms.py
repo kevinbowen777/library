50d4597397ba429d69123d79af672ae4cde0d1d7
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-10 09:48:54 -0700

    Remove flake8 pragmas

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,8 @@
-import datetime  # noqa:F401
+import datetime
 
 from django import forms
-from django.core.exceptions import ValidationError  # noqa:F401
-from django.utils.translation import gettext_lazy as _  # noqa:F401
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 class RenewBookForm(forms.Form):
