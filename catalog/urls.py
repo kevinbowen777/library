3185e4a6651db4e83777c1521878e8f7f6c5fa08
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-11 16:32:07 -0700

    Add about page URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
     path("books/", views.BookListView.as_view(), name="books"),
     path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     path("book/create/", views.BookCreate.as_view(), name="book-create"),
