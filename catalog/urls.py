3579ec0321da6b16059d391d7fed8e8227bda653
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-11 14:53:42 -0700

    Add BookDelete URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     path("book/create/", views.BookCreate.as_view(), name="book-create"),
     path("book/<int:pk>/update/", views.BookUpdate.as_view(), name="book-update"),
+    path("book/<int:pk>/delete/", views.BookDelete.as_view(), name="book-delete"),
     #
     path("authors/", views.AuthorListView.as_view(), name="authors"),
     path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
