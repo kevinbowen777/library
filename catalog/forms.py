7a7cfeb9a0c78a1bb511218a9687447d3fa1ead8
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-11 10:41:30 -0700

    Change to Model.form

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,10 @@ import datetime
 
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
+
+from .models import BookInstance
 
 
 class RenewBookForm(forms.Form):
@@ -23,3 +26,25 @@ class RenewBookForm(forms.Form):
 
         # Remember to always return the cleaned data.
         return data
+
+
+class RenewBookModelForm(ModelForm):
+    def clean_due_back(self):
+        data = self.cleaned_data["due_back"]
+
+        # Check if a date is not in the past.
+        if data < datetime.date.today():
+            raise ValidationError(_("Invalid date - renewal in past"))
+
+        # Check if a date is in the allowed range (+4 weeks from today).
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_("Invalid date - renewal more than 4 weeks ahead"))
+
+        # Remember to always return the cleaned data.
+        return data
+
+    class Meta:
+        model = BookInstance
+        fields = ["due_back"]
+        labels = {"due_back": _("Renewal date")}
+        help_texts = {"due_back": _("Enter a date between now and 4 weeks (default 3).")}
--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views.generic import DetailView, ListView
 
-from .forms import RenewBookForm
+from .forms import RenewBookModelForm
 from .models import Author, Book, BookInstance, Genre  # noqa:F401
 
 
@@ -75,21 +75,21 @@ class LoanedBooksAllListView(PermissionRequiredMixin, ListView):
 
 
 @login_required
-@permission_required("catalog.can_mark_returned", raise_exception=True)
+@permission_required("can_mark_returned", raise_exception=True)
 def renew_book_librarian(request, pk):
     """View function for renewing a specific BookInstance by librarian."""
     book_instance = get_object_or_404(BookInstance, pk=pk)
 
     if request.method == "Post":
-        form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
         if form.is_valid():
-            book_instance.due_back = form.cleaned_data["renewal_date"]
+            book_instance.due_back = form.cleaned_data["due_back"]
             book_instance.save()
 
             return HttpResponseRedirect(reverse("all-borrowed"))
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
+        form = RenewBookModelForm(initial={"due_back": proposed_renewal_date})
 
     context = {
         "form": form,
