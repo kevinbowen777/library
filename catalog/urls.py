fa252ddb2dbd8d766b7e1a9ebe0bf5c83a1b3269
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-11 11:44:17 -0700

    Add AuthorCreate URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     #
     path("authors/", views.AuthorListView.as_view(), name="authors"),
     path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
+    path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
     #
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path(r"borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),
-    #
     path("book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"),
 ]
