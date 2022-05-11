57861a8ddd17e30a39763e6ae3d2a595adf304d3
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-11 12:58:39 -0700

    Add AuthorDelete URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("author/<int:pk>", views.AuthorDetailView.as_view(), name="author-detail"),
     path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
     path("author/<int:pk>/update/", views.AuthorUpdate.as_view(), name="author-update"),
+    path("author/<int:pk>/delete/", views.AuthorDelete.as_view(), name="author-delete"),
     #
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path(r"borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),
