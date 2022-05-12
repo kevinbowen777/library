8732cc19951c087ec1408824853758ba83a96864
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-12 13:40:06 -0700

    Move home & about tests from catalog

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase  # noqa:F401
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
 
-# Create your tests here.
+from .views import AboutPageView, HomePageView
+
+
+class HomepageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_home_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "local library")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "About Page")
+
+    def test_home_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_home_page_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, "About Page")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Local Library Home")
+
+    def test_about_page_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
