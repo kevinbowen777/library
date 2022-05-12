6cd56fe39613a14a95a97659b77a9a7bca4eb835
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-12 12:34:51 -0700

    Move about URL to pages app

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.AboutPageView.as_view(), name="about"),
     path("books/", views.BookListView.as_view(), name="books"),
     path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
     path("book/create/", views.BookCreate.as_view(), name="book-create"),
