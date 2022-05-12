8c98fae39c724c3893229bdaaffb478125c9e144
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-12 12:39:25 -0700

    Add Home & AboutPageViews

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render  # noqa:F401
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
