ed6e8f2577c6247d65cdbc75a0a10a24767dc69a
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-13 19:00:05 -0700

    Add name lengths & absolute_url tests

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -18,3 +18,34 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         field_label = author._meta.get_field("last_name").verbose_name
         self.assertEqual(field_label, "last name")
+
+    def test_date_of_birth_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field("date_of_birth").verbose_name
+        self.assertEqual(field_label, "date of birth")
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field("date_of_death").verbose_name
+        self.assertEqual(field_label, "Died")
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field("first_name").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_last_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field("last_name").max_length
+        self.assertEqual(max_length, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = "{0}, {1}".format(author.last_name, author.first_name)
+
+        self.assertEqual(str(author), expected_object_name)
+
+    def test_get_absolute_url(self):
+        author = Author.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(author.get_absolute_url(), "/catalog/author/1")
