f472bc310e8aedf525d80f656c7fa782f4de69cd
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-13 20:50:55 -0700

    Change paginate_by to 10

--- catalog/views.py
+++ catalog/views.py
@@ -58,7 +58,7 @@ class AuthorDetailView(LoginRequiredMixin, DetailView):
 class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
     model = BookInstance
     template_name = "catalog/bookinstance_list_borrowed_user.html"
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact="o").order_by("due_back")
@@ -68,7 +68,7 @@ class LoanedBooksAllListView(PermissionRequiredMixin, ListView):
     model = BookInstance
     permission_required = "catalog.can_mark_returned"
     template_name = "catalog/bookinstance_list_borrowed_all.html"
-    paginate_by = 2
+    paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact="o").order_by("due_back")
