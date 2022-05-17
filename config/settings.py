90dfe8ac0845cc17ca8e2f337dd182b27af39d21
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-17 12:35:23 -0700

    Apply 3rd email settings. Fixes #10

--- config/settings.py
+++ config/settings.py
@@ -144,8 +144,15 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 )
+# third-party email relay configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = "kevin.bowen@gmail.com"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = "bootstrap4"
