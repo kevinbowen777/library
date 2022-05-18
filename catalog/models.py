451b63b440d9907b4c13bb2d41b904e1519c4473
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-17 18:24:02 -0700

    Add cover column for bookcovers

--- catalog/models.py
+++ catalog/models.py
@@ -41,6 +41,7 @@ class Book(models.Model):
     )
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     class Meta:
         ordering = ["title", "author"]
