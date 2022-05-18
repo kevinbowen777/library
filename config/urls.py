b33c5a894b520a1cf1f92f45d20af4dffbc1c941
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-17 18:25:27 -0700

    Add media URL paths

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -10,7 +11,7 @@ urlpatterns = [
     # Application specific
     path("", include("pages.urls")),
     path("catalog/", include("catalog.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar  # noqa: F401
