698a3d0d226d0c9e0eb7fd535ecf91827ea8f27f
Kevin Bowen <kevin.bowen@gmail.com>
2022-05-20 11:51:56 -0700

    Add search URL

--- catalog/urls.py
+++ catalog/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path("mybooks/", views.LoanedBooksByUserListView.as_view(), name="my-borrowed"),
     path(r"borrowed/", views.LoanedBooksAllListView.as_view(), name="all-borrowed"),
     path("book/<uuid:pk>/renew/", views.renew_book_librarian, name="renew-book-librarian"),
+    #
+    path("search/", views.SearchResultsListView.as_view()),
 ]
