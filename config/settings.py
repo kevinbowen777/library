f500f71cd5d298c9fa4b01ff0ad620c012557e70
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-25 16:46:40 -0700

    Fix Middleware syntax

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleWare",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
